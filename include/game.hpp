aa72205f6e00f5f89811290f3cc265c9bd078e2e
VSathvikReddy <vangapellisathvik@gmail.com>
2026-02-26 19:29:23 +0530

    Assets included

--- include/game.hpp
+++ include/game.hpp
@@ -1,6 +1,14 @@
+#pragma once
+#include <memory>
 
+#include "pieces.hpp"
+#include "board.hpp"
 
 class ChessEngine{
 private:
-    std::vector<
-}
+    Board board;
+    std::vector<std::unique_ptr<Piece>> Black_Pieces;
+    std::vector<std::unique_ptr<Piece>> White_Pieces;
+public:
+    ChessEngine();
+};
--- include/pieces.hpp
+++ include/pieces.hpp
@@ -5,11 +5,42 @@ struct Position{
     uint8_t pos;
 }; 
 
+std::vector<std::pair<uint8_t,uint8_t>> convert 
 
 class Piece{
-private:
+protected:
     Position pos;
+
+    bool has_moved=false;
+    bool captured = false;
 public:
     const Position getPos() const;
+    bool goTo(Position new_pos);
     virtual const std::vector<Position> getPossibleMoves() const;
 };
+
+
+class Pawn:public Piece{
+
+};
+
+class Rook:public Piece{
+
+};
+
+class Knight:public Piece{
+
+};
+
+class Bishop:public Piece{
+
+};
+
+class Queen:public Piece{
+
+};
+
+class King:public Piece{
+
+};
+
